bd7f5a6d912383b31cc5d0170014109e2095e4b7
zhiyong-y <15684187702@163.com>
2021-09-17 16:29:00 +0800

    clean

--- packages/sogCountTo/index.js
+++
@@ -1,9 +0,0 @@
-// 导入组件，组件必须声明 name
-import SogCountTo from "./main.vue";
-
-// 为组件添加 install 方法，用于按需引入
-SogCountTo.install = function (Vue) {
-  Vue.component(SogCountTo.name, SogCountTo);
-};
-
-export default SogCountTo;
